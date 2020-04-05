ed61d9c61b717d4f813de0e422f3937485f77fdb
TheodorJ <teddyjo@live.com>
2020-04-05 13:27:32 -0400

    Added control FSM for FPU Job Manager

--- fpu/fpu_jm.sv
+++ fpu/fpu_jm.sv
@@ -12,12 +12,49 @@ module FPUJobManager
    output logic done,
    output logic [3:0] port_ctr);
 
-  enum logic [5:0] {WAIT, LINEAR} state, nextState;
+  enum logic [5:0] {WAIT, LINEARFW, DONE} state, nextState;
 
  
   // Intermediate registers
   reg [31:0][31:0] r;
 
+
+  // Linear Forward control FSM
+  /*
+    input logic clk, rst_l,
+   mem_handle a, b, c, d,
+   input logic go,
+   output logic done,
+   inout reg[31:0][31:0] r
+  */
+
+  logic lfw_done;
+  LinearForward lf(.clk, .rst_l, .a, .b, .c, .d, .go(state == LINEARFW), 
+                   .done(lfw_done), .r);
+
+  // Next state logic
+  always_comb begin
+    unique case(state)
+      WAIT: begin
+        if(avail && op == LINEAR_FW)
+          nextState = LINEARFW;
+        else
+          nextState = WAIT;
+      end
+      LINEARFW:
+        if(lfw_done) begin
+          nextState = DONE;
+        else
+          nextState = LINEAR_FW;
+      end
+      DONE: begin
+        if(avail)
+          nextState = DONE;
+        else
+          nextState = WAIT;
+      end
+    endcase
+  end
 
   // FSM logic
   always_ff @(posedge clk, negedge rst_l) begin
